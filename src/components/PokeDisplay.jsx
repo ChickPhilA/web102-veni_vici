167195d5879da33726f6c7bd792a853c5482473c
Phil <natsudragoneel282003@gmail.com>
2026-06-26 18:07:28 -0700

    Created boilerplate to display Pokemon information

--- src/components/PokeDisplay.jsx
+++ src/components/PokeDisplay.jsx
@@ -1,6 +1,13 @@
+import '../App.css'
+
 const PokeDisplay = () => {
     return (
         <>
+            <div className="container">
+                <h1> Poké Generator </h1>
+                <p> Let us discover the world of Pokémon!</p>
+                <button className="generate-btn">Generate Pokémon! </button>
+            </div>
         </>
     )
 }
