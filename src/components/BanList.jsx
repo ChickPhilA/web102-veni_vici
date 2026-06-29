d170e67429454fbe6df7b8f0b209a882d2da63a6
Phil <natsudragoneel282003@gmail.com>
2026-06-29 15:07:21 -0700

    Added final functionality between banning and unbanning attributes

--- src/components/BanList.jsx
+++ src/components/BanList.jsx
@@ -1,9 +1,27 @@
-const BanList = () => {
+const BanList = ({banList, setBanList}) => {
+
+    const unbanAttribute = (attribute) => {
+        setBanList(
+            banList.filter((unbannedAttribute) =>
+                unbannedAttribute !== attribute
+        ))
+    }
+
+
     return(
         <>
             <div className="ban-container">
-
-                
+                <h2 className="attribute-info"> Attribute Ban-List </h2>
+                <h4 className="attribute-info"> All banned attributes will appear here. Click on an attribute to unban it.</h4>
+                    <div>
+                        {banList.map((attribute, index) => {
+                            return (
+                                <button className="attribute-btn" onClick={() => {unbanAttribute(attribute)}}>
+                                    {attribute}
+                                </button>
+                            )
+                        })}
+                    </div>
             </div>
         </>
     )
--- src/components/PokeDisplay.jsx
+++ src/components/PokeDisplay.jsx
@@ -11,6 +11,13 @@ const PokeDisplay = ({pokemon, fetchPokemon, banList, setBanList, randomMoveInde
         fairy: '🧚', normal: '⭐'
     }
 
+    const banAttribute = (attribute) => {
+
+        if(!banList.includes(attribute)) {
+            setBanList([...banList, attribute])   
+        }
+    }
+
     const matchType = (type) => `${type} ${typeEmojis[type] ?? '❓'}`
 
     const checkHiddenAbility = (ability) => {
@@ -18,30 +25,58 @@ const PokeDisplay = ({pokemon, fetchPokemon, banList, setBanList, randomMoveInde
         return ability.ability.name + hidden_emoji
     }
 
+    const removeDashMove = (moveObj) => {
+        return moveObj.replaceAll('-', ' ')
+    }
+
+    const removeDashAbility = (abilityName) => {
+        return abilityName.replaceAll('-', ' ')
+    }
+
+    const removeDashFromName = (pokemonName) => {
+        return pokemonName.replaceAll('-', ' ')
+    }
+
     return (
         <>
             <div className="container">
                 <h1> Poké Discovery </h1>
-                <p> Let us discover the world of Pokémon!</p>
-                <p> Click the button below to generate one Pokémon out of all 1028, from the National Pokédex!</p>
+                <h2> Let us discover the world of Pokémon!</h2>
+                <h2> Click the button below to generate one Pokémon out of all 1028, from the National Pokédex!</h2>
+                <h5> Attributes such as type(s), abilities, and a random move from the Pokémon's moveset will be displayed below.
+                    Click on an attribute to add it to a ban list, where a Pokémon with those attirbutes will not be generated.
+                </h5>
                     <div className="pokemon-display">
                         {/* This is the container where we'll place our Pokemon API */}
                         {pokemon ?
                                 <div>
                                     <img src={pokemon.sprites.front_default}></img>
-                                    <h2> {pokemon.name} </h2>
+                                    <h2 id="name"> {removeDashFromName(pokemon.name)} </h2>
                                     <div>
-                                        {pokemon.types.map((typeObj, index) => (
-                                            <button key={typeObj.type.name} className="attribute-btn"> {matchType(typeObj.type.name)} </button>
-                                            )
-                                        )}
+                                            {pokemon.types.map((typeObj, index) => (
+                                                    <button onClick={() => {
+                                                        banAttribute(typeObj.type.name)
+                                                    }} 
+                                                    key={typeObj.type.name} className="attribute-btn"> 
+                                                        {matchType(typeObj.type.name)} 
+                                                    </button>
+                                                )
+                                            )}
 
                                         {pokemon.abilities.map((abilityObj, index) => (
-                                            <button key={abilityObj.ability.name} className="attribute-btn"> {checkHiddenAbility(abilityObj)} 🧠 </button>
+                                                <button onClick={() => {
+                                                    banAttribute(abilityObj.ability.name)
+                                                }} key={abilityObj.ability.name} className="attribute-btn">
+                                                    {removeDashAbility(checkHiddenAbility(abilityObj))} 🧠
+                                                </button>
                                             )
                                         )}
 
-                                        <button className="attribute-btn"> {pokemon.moves[randomMoveIndex].move.name} 💥 </button>
+                                        <button onClick={() => 
+                                                banAttribute(pokemon.moves[randomMoveIndex].move.name)
+                                         } className="attribute-btn"> 
+                                                {removeDashMove(pokemon.moves[randomMoveIndex].move.name)} 💥
+                                        </button>
 
                                     </div>
                                 </div>
